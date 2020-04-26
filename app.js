b13ae7375f882a0c332902ff076ef75a2feaae44
unknown <ngudbhav05@hotmail.com>
2020-04-26 23:35:41 +0530

    Enable whatsapp integration

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var CronJob = require('cron').CronJob;
+const pool = require('./db');
+const accountSid = 'AC321211839a160ac721af1194062778e6';
+const authToken = 'ff2912cca4051e94e9cab173a28ba8d1';
+const client = require('twilio')(accountSid, authToken);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -26,6 +31,46 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+var job = new CronJob(
+  '* 30 9 * * *',
+  function () {
+    console.log('execute');
+    client.messages
+      .create({
+        from: 'whatsapp:+14155238886',
+        body: 'Generate your report here: https://nosegay-attendance.herokuapp.com/output',
+        to: 'whatsapp:+917014878106'
+      }).then((message) => {
+        console.log(message.sid);
+        pool.getConnection(function (error, db) {
+          if (error) throw error;
+          else {
+            db.query('update 9s set time = NULL, status = 0', (error, results) => {
+              if (error) throw error;
+            });
+            db.release();
+          }
+        });
+      }).catch((error) => {
+        console.log(error);
+        pool.getConnection(function (error, db) {
+          if (error) throw error;
+          else {
+            db.query('update 9s set time = NULL, status = 0', (error, results) => {
+              if (error) throw error;
+            });
+            db.release();
+          }
+        });
+        throw error;
+      });
+  },
+  null,
+  true,
+  'Asia/Kolkata'
+);
+job.start();
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var pdf = require("pdf-creator-node");
+const fs = require('fs');
+var html = fs.readFileSync('./template.html', 'utf8');
+var options = {
+  format: "A3",
+  orientation: "portrait",
+  border: "10mm",
+  header: {
+    height: "45mm",
+    contents: '<div style="text-align: center;">Author: NGUdbhav</div>'
+  },
+  "footer": {
+    "height": "28mm",
+    "contents": {
+      first: 'Cover page',
+      2: 'Second page', // Any page number is working. 1-based index
+      default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+      last: 'Last Page'
+    }
+  }
+};
 const pool = require('../db.js');
 const moment = require('moment');
 require('moment-timezone');
@@ -12,10 +33,38 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/output', (req, res, next) => {
+  pool.getConnection(function (error, db) {
+    if (error) throw error;
+    else {
+      db.query('Select * from 9s where status = 1', (error, results) => {
+        if (error) throw error;
+        else {
+          db.release();
+          var document = {
+            html: html,
+            data: {
+              users: results
+            },
+            path: "../output.pdf"
+          };
+          pdf.create(document, options)
+            .then(resi => {
+              res.download('../output.pdf');
+            })
+            .catch(error => {
+              console.error(error)
+            });
+        }
+      });
+    }
+  });
+});
+
 router.post('/markAttendance', (req, res, next) => {
   let name = req.body.name.toUpperCase();
   let time = req.body.time
-  //Allow Attendance Mark before 9:30 AM everyday 
+  //Allow Attendance Mark before 9:30 AM and after 9:00 AM everyday 
   if (time <= threshold && time >= minimum){
     pool.getConnection(function (error, db) {
       if (error) throw error;
@@ -42,7 +91,7 @@ router.post('/markAttendance', (req, res, next) => {
               }
             }
             else {
-              res.send('No Student found with the name. Make sure you typed the name as per the school records!');
+              res.send('No Student found with the name. Make sure you type the name as per the school records!');
               db.release();
             }
           }
